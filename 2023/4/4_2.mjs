447cdee64d5057780628c244f9de6b8c3d56636f
Edward Hurst <Edward.Hurst@aexp.com>
2023-12-04 17:48:16 +0000

    2023 day 4

--- 2023/4/4_2.mjs
+++ 2023/4/4_2.mjs
@@ -1,15 +1,6 @@
-const partOne = (data) => {
-  const realLength = data.length;
-
-  console.info('Starting length:', data.length);
-
+const partTwo = (data) => {
   for(let i = 0; i < data.length; i = i + 1) {
     const cardIndex = [...data[i].matchAll(/Card\W*(\d*)/g)].map(x => parseInt(x[1]))[0] - 1;
-    
-    // if (i > realLength) console.info('Rerunning card:', cardIndex);
-
-    console.info(cardIndex);
-
     const [results, numbers] = data[cardIndex].replace(/^Card\W*\d+: /g, '').split('| ');
     
     const winning = {};
@@ -24,29 +15,23 @@ const partOne = (data) => {
         multiplier += 1;
       }
     });
-
-    // console.info('Line', i, multiplier)
     
     if (multiplier) {
-      // console.info('CARD: ', cardIndex, 'WINS', multiplier);
-
       for (let j = cardIndex + 1 ; j < cardIndex + 1 + multiplier; j += 1) {
-        // console.info('WINNER: ', j);
         data.push(data[j])
       }
     }    
   }
 
-  console.info('Ending length:', data.length);
+  return data.length;
 }
 
 import fs from 'fs';
 
-const input = fs.readFileSync('./input.txt', 'utf8');
+const input = fs.readFileSync('./test.txt', 'utf8');
 const data = input.split(/\n/);
 
-const pOne = partOne(data);
-// const pTwo = partTwo();
+const results = partTwo(data);
 
 console.info('====================')
-console.info(pOne);
+console.info(results);
